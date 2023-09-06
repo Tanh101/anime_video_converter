c5c5881cd8065ea429cf4090f06ecc2842fb3b7d
VanTanh <lytanh@starboard-asia.com>
2023-09-07 01:03:42 +0700

    register ui

--- auth_site/views.py
+++ auth_site/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def login(request):
     return render(request, "auth_site/login.html")
+def register(request):
+    return render(request, "auth_site/register.html")
