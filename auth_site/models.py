18b4a9d70310346ffafbe3e086a35b83c12cc102
changchang240402 <thuytrangdao240402@gmail.com>
2023-09-07 00:44:19 +0700

    setup model and docker-compose

--- auth_site/models.py
+++ auth_site/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class User(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Email = models.CharField(max_length=50)
+    Password = models.CharField(max_length=50)
+    Role = models.BooleanField()
+    Status = models.CharField(max_length=25)
+    Created_at = models.DateTimeField(auto_now_add=True)
+    Update_at = models.DateTimeField(auto_now=True)
+    use_in_migrations = True
+    
--- home/models.py
+++ home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Video(models.Model):
+    Id = models.AutoField(primary_key=True)
+    User_Id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='videos', null=True)
+    Original_video_path = models.CharField(max_length=50, blank=True, null=True)
+    Converted_video_path = models.CharField(max_length=50, blank=True, null=True)
+    Status = models.CharField(max_length=25)
+    Created_at = models.DateTimeField(auto_now_add=True)
+    Update_at = models.DateTimeField(auto_now=True)
+    use_in_migrations = True
+
+class History(models.Model):
+    Id = models.AutoField(primary_key=True)
+    User_Id = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='histories')
+    Action = models.CharField(max_length=25)
+    Created_at = models.DateTimeField(auto_now_add=True)
+    Update_at = models.DateTimeField(auto_now=True)
+    use_in_migrations = True
