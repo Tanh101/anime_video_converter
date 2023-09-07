bb05a9ecb94a491898bcbbd899ee1fe9ceb0a0a3
Quoc Khanh <nguyenkimt322@gmail.com>
2023-09-07 16:41:00 +0700

    register

---
+++ auth_site/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import MyUser
+
+
+
+class RegistrationForm(forms.Form):
+    print(forms)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    print(password)
+    confirm_password = forms.CharField(widget=forms.PasswordInput, label="confirm_password")
+    # print(confirm_password)
+    def clean(self):
+        cleaned_data = super().clean()
+        email = self.cleaned_data.get('email')
+        if MyUser.objects.filter(Email=email).exists():
+            raise forms.ValidationError("Email đã tồn tại.")
+        password = cleaned_data.get("password")
+        print(password)
+        confirm_password = cleaned_data.get("confirm_password")
+        print(confirm_password)
+        if password != confirm_password:
+            raise forms.ValidationError("Mật khẩu không khớp.")
