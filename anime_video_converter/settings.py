fed6abc691208f0ba1260bbb0c92a6ea15014a7a
VanTanh <lytanh@starboard-asia.com>
2023-09-07 13:57:46 +0700

    update logo

--- anime_video_converter/settings.py
+++ anime_video_converter/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "dist"]
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
