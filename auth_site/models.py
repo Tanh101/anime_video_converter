082dd1b710c180f21e8acbc00a4e585ba4d08f6c
Quoc Khanh <nguyenkimt322@gmail.com>
2023-09-07 16:41:00 +0700

    register

--- auth_site/models.py
+++ auth_site/models.py
@@ -3,11 +3,11 @@ from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-class User(models.Model):
+class MyUser(models.Model):
     Id = models.AutoField(primary_key=True)
     Email = models.CharField(max_length=50)
     Password = models.CharField(max_length=50)
-    Role = models.BooleanField()
+    Role = models.BooleanField(default=True)
     Status = models.CharField(max_length=25)
     Created_at = models.DateTimeField(auto_now_add=True)
     Update_at = models.DateTimeField(auto_now=True)
--- auth_site/views.py
+++ auth_site/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def login(request):
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from .models import MyUser
+def login(request): 
     return render(request, "auth_site/login.html")
+from django.contrib.auth.hashers import make_password
+
 def register(request):
-    return render(request, "auth_site/register.html")
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            # Sử dụng make_password để mã hóa mật khẩu
+            hashed_password = make_password(password)
+
+            # Tạo người dùng mới và lưu vào cơ sở dữ liệu với mật khẩu đã mã hóa
+            user = MyUser(Email=email, Password=hashed_password)
+            user.save()
+
+            # Chuyển hướng sau khi đăng ký thành công
+            return redirect('login')  # Điều hướng đến trang đăng nhập (cần thiết phải có URL 'login')
+        else:
+            print(form.errors)
+
+    else:
+        form = RegistrationForm()
+
+    return render(request, "auth_site/register.html", {'form': form})
