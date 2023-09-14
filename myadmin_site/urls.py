39d2b4f2cc250d666abd06a8c89715dea1897218
Nguyen Khac Thai <zed999cz@gmail.com>
2023-09-14 17:04:42 +0700

    Ban function

--- myadmin_site/urls.py
+++ myadmin_site/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("users/", myadmin_site.user_list, name="api_users"),    # Endpoint for fetching user data
     path("users/<int:id>/delete/", myadmin_site.delete_user, name="api_delete_user"),  # Endpoint for deleting a user by ID
     path("videos/<int:id>/detail/", myadmin_site.vide_detail, name="api_video_detail"),  # Endpoint for fetching video details by ID
+    path('users/<int:id>/ban/', myadmin_site.ban_user, name='ban_user'),
 ]
