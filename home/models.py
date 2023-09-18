670e470b6cfa2b3ec87faa499176bd73bcdd9bbe
VanTanh <lytanh@starboard-asia.com>
2023-09-18 17:12:20 +0700

    resolve conflict

--- home/models.py
+++ home/models.py
@@ -6,18 +6,11 @@ from auth_site.models import MyUser
 
 class Video(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='videos', null=True)
-    original_video_path = models.CharField(max_length=50, blank=True, null=True) 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_video', null=True)
+    name = models.CharField(max_length=100)
+    original_video_path = models.CharField(max_length=50, blank=True, null=True)
+    converted_video_path = models.CharField(max_length=50, blank=True, null=True)
     status = models.CharField(max_length=25)
-    created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
-    use_in_migrations = True
-
-class History(models.Model):
-    id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='histories')
-    converted_video_path = models.CharField(max_length=50, blank=True, null=True) 
-    action = models.CharField(max_length=25)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     use_in_migrations = True
